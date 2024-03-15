745b17444c505d7fe34cbb358e6e6cd006962ddc
ANURAG PIPRIYA <anurag.2201230ec@iiitbh.ac.in>
2024-03-15 19:12:20 +0530

    Create conversion_operation.js

---
+++ conversion_operation.js
@@ -0,0 +1,19 @@
+let score=33
+score="33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber );
+
+let valueInString=String(score)
+console.log(typeof valueInString );
+
+//"33"--> 33
+//"33abc"-->NaN
+//true-->1, false-->0
+
+let somenumber=33
+let someNumIsString=String(somenumber)
+console.log(someNumIsString);
+console.log(typeof someNumIsString);
