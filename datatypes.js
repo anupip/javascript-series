9a1e292af5a17df1f12f5b679e25967a7ad05c8e
ANURAG PIPRIYA <anurag.2201230ec@iiitbh.ac.in>
2024-03-15 18:41:03 +0530

    variables

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+let naam="ANURAG"; //string
+let age=18 //num
+let isLggedIn=true //bool
+console.table([naam,age,isLggedIn]);
+
+console.log(typeof "anurag");
---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountId = 332843
+let accountEmail="anurag@gmail.com"
+var accountPassword ="23456"
+accountCity="jhansi"
+let ann //undefined
+accountEmail="anu.pip@gmail.com"
+//accountId="2"
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword,ann])
