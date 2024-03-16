28d7c930d6a17263ee5de879d48f49808ba916c7
ANURAG PIPRIYA <anurag.2201230ec@iiitbh.ac.in>
2024-03-16 12:58:07 +0530

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,17 @@
+console.log(0>1)
+console.log(6>1)
+console.log(0==0)
+console.log(0==1)
+console.log(0!=1)
+
+console.log(null > 0);
+console.log(null == 0); //equality check
+console.log(null >= 0);
+// comparison will convert null to a number(0)
+
+
+//UNDEFINED will always give output as false
+
+
+console.log("2" === 2);
+console.log(2 === 2);// === will check the value as well as its data type
