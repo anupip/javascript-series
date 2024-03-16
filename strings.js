c1bef69ed07c327d8c10950ed796dc3826fa2935
ANURAG PIPRIYA <anurag.2201230ec@iiitbh.ac.in>
2024-03-16 19:53:19 +0530

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,25 @@
+const name="anurag"
+const repoCount=60
+ 
+// console.log(name + repoCount + "value"); ----------->OUTDATED
+
+// STRING INTERPOLLATION
+console.log(`hello my name is ${name} and my repo count is ${repoCount} `);
+
+//declare string by constructor
+const strName = new String('anurag')
+console.log(strName);
+console.log(strName[0]);
+console.log(strName[1]);
+
+console.log(strName.length);
+console.log(strName.toUpperCase());
+console.log(strName.charAt(4));
+console.log(strName.indexOf("g"));
+
+console.log(strName.substring(1,5));
+
+const newone="    pipriya     "
+console.log(newone);
+console.log(newone.trim());
+
